f3d5b23981e039ea46960d2dd4e9a421baf9f9b4
owaise <awais.muhammad.jans@gmail.com>
2024-04-26 19:59:45 +0500

    first commit

---
+++ app/src/main/java/com/jexample/sample/picture/firstnametest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jexample.sample.picture.firstnametest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
