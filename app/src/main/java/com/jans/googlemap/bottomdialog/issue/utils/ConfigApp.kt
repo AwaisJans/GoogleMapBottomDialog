844ed0735ab8825c6e5ef8896fdc2d27ca2d6a77
owaise <awais.muhammad.jans@gmail.com>
2024-04-27 18:51:04 +0500

    Added extra code for email tel fax in webview

--- app/src/main/java/com/jans/googlemap/bottomdialog/issue/utils/ConfigApp.kt
+++ app/src/main/java/com/jans/googlemap/bottomdialog/issue/utils/ConfigApp.kt
@@ -1,8 +1,17 @@
 package com.jans.googlemap.bottomdialog.issue.utils
 
+import android.content.Context
+import android.widget.Toast
+
 class ConfigApp {
     companion object{
         const val BASE_URL_MARKER = "https://www.schlier.de/index.php?id=279&baseColor=272727&baseFontSize=14&action=getGeomap2Items"
         const val TYPE_MARKER = "marker"
+
+
+        fun showToast(msg:String,context:Context){
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
